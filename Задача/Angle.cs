776ddfb72fd6f293c82ab6130bc63c025180bbf3
BR <244097@niuitmo.ru>
2024-03-05 12:11:32 +0300

    Добавьте файлы проекта.

---
+++ Задача/Angle.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача
+{
+    internal class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+        public bool isCorrect = true;
+
+        public int Gradus { 
+            get
+            { 
+                return gradus; 
+            } 
+            set 
+            {
+                if (value >0)
+                {
+                    gradus = value;
+                }
+                else
+                {
+                    isCorrect = false;
+                }
+            } 
+        }
+
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value > 0 )
+                {
+                    min = value;
+                }
+                else
+                {
+                    isCorrect = false;
+                }
+            }
+        }
+
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    sec = value;
+                }
+                else
+                {
+                    isCorrect = false;
+                }
+            }
+        }
+
+
+        public Angle(int gradus, int min, int sec)
+            {
+            this.Sec = sec;
+            this.Min = min;
+            this.Gradus = gradus;
+            }
+
+    public double ToRadians ()
+        {
+            return ((double)Gradus + (double)Min / 60 + (double)Sec / 3600)*Math.PI/180;
+        }
+    }
+}
---
+++ Задача/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle angle;
+            do
+            {
+                Console.WriteLine("Введите данные");
+                int gradus = Convert.ToInt32(Console.ReadLine());
+                int min = Convert.ToInt32(Console.ReadLine());
+                int sec = Convert.ToInt32(Console.ReadLine());
+                angle = new Angle(gradus, min, sec);
+            }
+            while (angle.isCorrect == false);
+
+            double radians = angle.ToRadians();
+            Console.WriteLine(radians);
+            Console.ReadKey();
+        }
+    }
+}
